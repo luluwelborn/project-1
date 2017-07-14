16b38290a6064cf07bb298ce55f0fda8e9cd7d3c
Lauren Welborn <laurenwelborn@Laurens-MacBook-Pro.local>
2017-07-14 11:48:09 -0600

    collision detected

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-var obstacle = $(".obstacle");
+var obstacle = $(".largeObstacleLeft");
 var score = $(".score");
 var boundingBox = $(".boundingBox")
 
@@ -44,6 +44,30 @@ function start()	{
 	});
 }
 
+
+/* detect collision */ 
+function collision($player, $largeObstacleLeft) {
+    // console.log("reading collision");
+      // coordinates of the div
+      var x1 = $player.offset().left;
+      var y1 = $player.offset().top;
+      var x2 = $largeObstacleLeft.offset().left;
+      var y2 = $largeObstacleLeft.offset().top;
+      if ((y1 + $player.outerHeight(true)) < y2 ||
+        y1 > (y2 + $largeObstacleLeft.outerHeight(true)) ||
+        (x1 + $player.outerWidth(true)) < x2  ||
+        x1 > (x2 + $largeObstacleLeft.outerWidth(true)))
+        return false;
+        window.alert("A cloud woke you up! Game Over");
+        console.log("collision!");
+      return true;
+    }
+
+// set times to look for collision
+window.setInterval(function() {
+    console.log("running at intervals");
+    $('#result').text(collision($('.player'), $('.largeObstacleLeft')));
+}, 200);
 
 
 
