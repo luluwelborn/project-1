f4ea7311ec6b0c59073059177c03df9bf2e42396
Lauren Welborn <laurenwelborn@Laurens-MacBook-Pro.local>
2017-07-17 12:41:21 -0600

    images

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
 
 function start()	{
   // Animations
-	$(".largeObstacleLeft").css("animation", "move 6s infinite", "transition-timing-function:linear");
+	$(".largeObstacleLeft").css("animation", "move 6s infinite");
   // $(".largeObstacleLeft").css("animation-timing-function:linear");
-  $(".largeObstacleRight").css("animation", "move 6s infinite", "transition-timing-function:linear");
+  $(".largeObstacleRight").css("animation", "move 6s infinite");
   $(".largeObstacleRight").css("animation-delay", "2s");
   // $(".largeObstacleLeft").css("animation-timing-function:linear");
 
